aa685ca9ab4e913712fa2e10c59a6ae407f3d9f7
Audwitiya Bandyopadhyay <audwitiyabandyopadhyay74@outlook.com>
2024-11-11 21:38:25 +0530

    Update NavBar.jsx

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -60,51 +60,51 @@ const NavBar = () => {
     setCustomStyle((prevStyle) => (prevStyle === "block" ? "hidden" : "block"));
   };
 
+  const inactiveClass = "h-[4.5rem] ease-in-out duration-150 hover:text-red-600 border-[#0000] border-b-[5px] hover:border-b-red-600 rounded-md flex justify-center items-center w-[4rem] flex-col";
+  const activeClass = "h-[4.5rem] ease-in-out duration-150 text-red-600 border-[#0000] border-b-[5px] border border-b-red-600 rounded-md flex justify-center items-center w-[4rem] flex-col";
+
   if (user === null) {
     return (
-      <nav className='h-16 w-screen bg-white text-black flex items-center shadow-md relative'>
-        <div className="logo flex items-center justify-center gap-2 w-[20%]">
-          <Image src={Logo} alt="logo" className='w-14 h-14' />
-          <h1 className='text-2xl font-bold'>Porichoi</h1>
+      <nav className='h-16 w-full bg-white text-black flex items-center shadow-md relative px-4 sm:px-8 lg:px-12'>
+        <div className="logo flex items-center justify-center gap-2 w-[30%] md:w-[20%]">
+          <Image src={Logo} alt="logo" className='w-10 h-10 md:w-14 md:h-14' />
+          <h1 className='text-lg md:text-2xl font-bold'>Porichoi</h1>
         </div>
-        <div className="h-max w-[60%] flex items-center gap-4 justify-center"></div>
-        <div className="w-[20%] h-[4rem] flex align-middle gap-4 items-center">
-          <button className='w-[7vw] h-[6vh] bg-[#ebeaea] hover:scale-110 rounded-md' onClick={() => { document.location.href = "/log-in" }}>Login</button>
-          <button className='w-[7vw] h-[6vh] bg-[#fff] hover:scale-110 rounded-md' onClick={() => { document.location.href = "/sign-up" }}>Sign up</button>
+        <div className="hidden md:flex w-[60%] items-center gap-4 justify-center"></div>
+        <div className="w-[70%] md:w-[20%] flex justify-end gap-2 md:gap-4">
+          <button className='w-[7vw] md:w-[6vw] h-[6vh] bg-[#ebeaea] hover:scale-105 rounded-md' onClick={() => { document.location.href = "/log-in" }}>Login</button>
+          <button className='w-[7vw] md:w-[6vw] h-[6vh] bg-[#fff] hover:scale-105 rounded-md' onClick={() => { document.location.href = "/sign-up" }}>Sign up</button>
         </div>
       </nav>
     );
   }
 
-  const inactiveClass = "h-[4.5rem] ease-in-out duration-150 hover:text-red-600 border-[#0000] border-b-[5px] hover:border-b-red-600 rounded-md flex justify-center items-center w-[4rem] flex-col";
-  const activeClass = "h-[4.5rem] ease-in-out duration-150 text-red-600 border-[#0000] border-b-[5px] border border-b-red-600 rounded-md flex justify-center items-center w-[4rem] flex-col";
-
   return (
     <>
-      <nav className='h-16 w-screen bg-white text-black flex items-center shadow-md fixed z-100'>
-        <div className="logo flex items-center justify-center gap-2 w-[20%]">
-          <Image src={Logo} alt="logo" className='w-14 h-14' />
-          <h1 className='text-2xl font-bold'>Porichoi</h1>
+      <nav className='h-16 w-full bg-white text-black flex items-center shadow-md fixed top-0 left-0 right-0 z-50 px-4 sm:px-8 lg:px-12'>
+        <div className="logo flex items-center justify-center gap-2 w-[30%] md:w-[20%]">
+          <Image src={Logo} alt="logo" className='w-10 h-10 md:w-14 md:h-14' />
+          <h1 className='text-lg md:text-2xl font-bold'>Porichoi</h1>
         </div>
-        <div className="h-max w-[60%] flex items-center gap-4 justify-center">
+        <div className="hidden md:flex w-[60%] items-center gap-4 justify-center">
           <Link href="/" className={home ? activeClass : inactiveClass}>
-            <i className='fa-solid fa-house' style={{ fontSize: "30px" }}></i>
+            <i className='fa-solid fa-house' style={{ fontSize: "24px" }}></i>
             <span style={{ fontSize: "10px" }}>Home</span>
           </Link>
           <Link href="/profile" className={profile ? activeClass : inactiveClass}>
-            <i className='fa-solid fa-user' style={{ fontSize: "30px" }}></i>
+            <i className='fa-solid fa-user' style={{ fontSize: "24px" }}></i>
             <span style={{ fontSize: "10px" }}>Profile</span>
           </Link>
           <Link href="/news" className={news ? activeClass : inactiveClass}>
-            <i className='fa-solid fa-newspaper' style={{ fontSize: "30px" }}></i>
+            <i className='fa-solid fa-newspaper' style={{ fontSize: "24px" }}></i>
             <span style={{ fontSize: "10px" }}>News</span>
           </Link>
         </div>
-        <div className="w-[20%] h-[4rem] flex align-middle justify-center gap-4 items-center">
-          <input type="text" placeholder='Search' className='h-[60%] ease-in-out duration-500 right-[-180%] absolute w-[80%] outline-none border border-black rounded-md p-2' />
-          <i className="fa-solid fa-cloud-arrow-up" style={{ fontSize: "24px" }} onClick={togglePostFormDisplay}></i>
-          <div className="w-[4vh] bg-[#ebe9e9] rounded-[100%] h-[4vh] flex flex-col items-center justify-center align-middle scale-125 absolute right-[20px] hover:cursor-pointer">
-            <FaSearch size={"20px"} onClick={toggleSearchInput} onClickCapture={tptosearch} />
+        <div className="w-[70%] md:w-[20%] flex justify-end items-center gap-2 md:gap-4">
+          <input type="text" placeholder='Search' className='hidden md:block h-[60%] ease-in-out duration-500 right-[-180%] absolute w-[80%] outline-none border border-black rounded-md p-2' />
+          <i className="fa-solid fa-cloud-arrow-up hidden md:block" style={{ fontSize: "24px" }} onClick={togglePostFormDisplay}></i>
+          <div className="md:w-[4vh] md:bg-[#ebe9e9] rounded-[100%] h-[4vh] flex items-center justify-center cursor-pointer">
+            <FaSearch size={"20px"} onClick={toggleSearchInput} />
           </div>
         </div>
       </nav>
