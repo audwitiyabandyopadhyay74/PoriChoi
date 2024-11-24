f0ff4c4a637832a5524d718b7d3833b1471cd3e0
Audwitiya Bandyopadhyay <audwitiyabandyopadhyay74@outlook.com>
2024-11-24 23:05:34 +0530

    Har Har Mahadev

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -43,9 +43,9 @@ if(user === null){
   return(
     <div className='w-screen h-screen flex flex-col gap-1 justify-center items-center  font-bold'>
             <NavBar />
-<span className="text-3xl">Please Login To Access This Page</span><a href='/log-in'>
-<button className='w-[10vw] text-white font-bold h-[3vw] bg-[#000] rounded-md'>Login</button></a>
-    </div>
+<span className="text-3xl  h-[10vh] w-max">Please Login To Access This Page</span><a href='/log-in'>
+<button className='lg:w-[10vw] text-white font-bold lg:h-[3vw] bg-[#000] rounded-md w-max h-[6vh] p-[10px] ' >Login</button></a>
+</div>
   )
 } else{ 
   return (
