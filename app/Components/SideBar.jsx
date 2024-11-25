8dddfeb985daae09374df9e9cbdbf2e44dafc9f8
Audwitiya Bandyopadhyay <audwitiyabandyopadhyay74@outlook.com>
2024-11-25 11:19:11 +0530

    Har Har Mahadev

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -1,66 +1,62 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { auth } from "../firebase";
-import { signOut } from "firebase/auth";
+import { auth } from '../firebase';
+import { signOut } from 'firebase/auth';
 import { usePathname } from 'next/navigation';
 
 const handleSignOut = () => {
-    signOut(auth).then(() => {
-        document.location.href = '/log-in';
-})};
+  signOut(auth).then(() => {
+    document.location.href = '/log-in';
+  });
+};
 
 const SideBar = () => {
-    const [profile, setProfile] = useState(false);
-    const [settings, setSettings] = useState(false);
-    const [yourPosts, setYourPosts] = useState(false);
-    const pathname = usePathname();
+  const [profile, setProfile] = useState(false);
+  const [settings, setSettings] = useState(false);
+  const [yourPosts, setYourPosts] = useState(false);
+  const pathname = usePathname();
 
-    useEffect(() => {
-        if (pathname === "/profile") {
-            setProfile(true);
-        } else if (pathname === "/profile/yourposts") {
-            setYourPosts(true);
-        } else {
-            setProfile(false);
-            setSettings(true);
-        }
-    }, [pathname]);
+  useEffect(() => {
+    if (pathname === '/profile') {
+      setProfile(true);
+      setSettings(false);
+      setYourPosts(false);
+    } else if (pathname === '/profile/yourposts') {
+      setProfile(false);
+      setSettings(false);
+      setYourPosts(true);
+    } else if (pathname === '/profile/settings') {
+      setProfile(false);
+      setSettings(true);
+      setYourPosts(false);
+    } else {
+      setProfile(false);
+      setSettings(false);
+      setYourPosts(false);
+    }
+  }, [pathname]);
 
-    const notActive = "flex items-center justify-center bg-black rounded-md gap-4 font-semibold text-white p-4";
-    const active = "flex items-center justify-center bg-white text-black border-r-[6px] p-4 border-r-white gap-4 font-semibold";
+  const notActive = "w-full py-4 flex items-center justify-center bg-gray-800 text-white font-semibold";
+  const active = "w-full py-4 flex items-center justify-center bg-white text-gray-800 font-semibold border-l-4 border-gray-800";
 
-    return (
-        <div className="flex flex-col bg-black h-full w-[20%] sm:w-screen sm:h-[24vh] lg:w-[15%] fixed left-0 rounded-md shadow-lg">
-            <ul className="flex flex-col gap-6 py-6 px-4">
-                <li
-                    className={profile ? active : notActive}
-                    onClick={() => { document.location.href = "/profile"; }}
-                >
-                    <i className='fa-solid fa-user' style={{ fontSize: "24px" }}></i> Your Profile
-                </li>
-                <li
-                    className={settings ? active : notActive}
-                    onClick={() => { document.location.href = "/profile/settings"; }}
-                >
-                    <i className='fa-solid fa-gear' style={{ fontSize: "24px" }}></i> Settings
-                </li>
-                <li
-                    className={yourPosts ? active : notActive}
-                    onClick={() => { document.location.href = "/profile/yourposts"; }}
-                >
-                    <i className='fa-solid fa-signs-posts' style={{ fontSize: "24px" }}></i> Your Posts
-                </li>
-                <li className="flex justify-center mt-auto">
-                    <button
-                        onClick={handleSignOut}
-                        className="w-[70%] h-10 bg-red-600 rounded-md text-white font-semibold"
-                    >
-                        Sign out
-                    </button>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="flex flex-col sm:flex-row h-full sm:h-screen w-full sm:w-1/4 bg-gray-800 sm:rounded-md">
+      <ul className="w-full">
+        <li className={profile ? active : notActive} onClick={() => { document.location.href = '/profile' }}>
+          <i className='fa-solid fa-user' style={{ fontSize: '24px' }}></i> Your Profile
+        </li>
+        <li className={settings ? active : notActive} onClick={() => { document.location.href = '/profile/settings' }}>
+          <i className='fa-solid fa-gear' style={{ fontSize: '24px' }}></i> Settings
+        </li>
+        <li className={yourPosts ? active : notActive} onClick={() => { document.location.href = '/profile/yourposts' }}>
+          <i className='fa-solid fa-signs-posts' style={{ fontSize: '24px' }}></i> Your Posts
+        </li>
+        <li className="w-full py-4 flex items-center justify-center bg-gray-800 pt-20">
+          <button onClick={handleSignOut} className='w-2/3 sm:w-1/2 py-2 bg-red-600 rounded-md text-white font-semibold'>Sign out</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
