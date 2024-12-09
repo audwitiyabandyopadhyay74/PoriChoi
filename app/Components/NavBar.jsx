d60e59c0e5ea495be8690e32b89056b379382793
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-09 21:53:31 +0530

    Har Har Mahadev

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -167,7 +167,7 @@ const NavBar = () => {
           </div>
         </div>
       )}
-                  <div className="hideshow h-max w-max absolute top-[4rem] right-[10px] py-5 p-4 bg-white rounded-md ">
+                  <div className="hideshow h-max w-max absolute top-[4rem] right-[1px] py-5 p-4 bg-white rounded-md ">
                     <ul style={{ listStyleType: "none" }}  className="flex flex-col gap-1">
                       <Link href={"/my-account"}><li><i className="fa-solid fa-user" style={{ fontSize: "20px" }}></i> My Account</li></Link>
                       <Link href={"/my-account/settings"}><li><i className="fa-solid fa-gear" style={{ fontSize: "20px" }}></i>Settings</li></Link>
