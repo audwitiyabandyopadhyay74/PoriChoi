33d3c20764132c3fd883d0a12250c8a6386a0bb8
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-09 17:10:48 +0530

    Har Har Mahadev

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -87,7 +87,7 @@ const NavBar = () => {
                     <Image
                       src={user.photoURL || Avatar}
                       alt="User Avatar"
-                      className="rounded-full w-10 h-10 bg-[#414040]"
+                      className="rounded-full w-10 h-10 "
                     />
                   </summary>
                   <p>
