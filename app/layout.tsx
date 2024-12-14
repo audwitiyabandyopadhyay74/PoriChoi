d03823d76533621d7b807ee688e3a4f6f17bf8a2
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-14 22:08:49 +0530

    Har Har Mahadev

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import localFont from "next/font/local";
 import "./globals.css";
-import footer from "./Components/Footer"
+// import footer from "./Components/Footer"
 import Footer from "./Components/Footer";
 
 const geistSans = localFont({
