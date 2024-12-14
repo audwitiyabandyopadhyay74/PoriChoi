583114c7a9a87d3d025d4f2b21906580d43f1e5a
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-14 19:41:05 +0530

    Har Har Mahadev

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -142,7 +142,7 @@ Upload
 
       {/* Mobile Navbar */}
       <nav className="md:hidden fixed bottom-0 left-0 right-0 h-16 bg-white shadow-t-md z-50">
-        <div className="flex items-center justify-around h-full">
+        <div className="flex items-center justify-evenly h-full">
           {user && ["home", "my-account", "news"].map((page) => (
             <Link
               key={page}
