aec96895d817fa190af833b48c1459180cda44c3
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-17 22:52:04 +0530

    Har Har Mahadev

--- app/Components/Moblie Nav.jsx
+++ app/Components/Moblie Nav.jsx
@@ -57,13 +57,13 @@ if(user){
                       onClick={toggleMenuVisibility}
                     />
              <i class="fa-solid fa-caret-up rotate-180" onClick={toggleMenuVisibility} id="icon1" style={{fontSize:"20px"}}></i>
-                </div>
+                </div>              </div>
+
                 <div className="search">
           <Link href="/search">
             <FaSearch size={25} />
           </Link>
         </div>
-              </div>
               </div>
               </div>
 
