90b0922fd93e01c1aaa11c6eb91d207dc946c009
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-17 09:37:48 +0530

    Har Har Mahadev

--- app/Components/Moblie Nav.jsx
+++ app/Components/Moblie Nav.jsx
@@ -1,59 +1,49 @@
 "use client";
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../favicon.ico'
 import Image from 'next/image';
-import {onAuthStateChanged} from "firebase/auth"
+import { onAuthStateChanged } from "firebase/auth"
 import Avatar from '../download.png'
-import {FaSearch} from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import Link from "next/link";
 
+const MobileNav = () => {
+  const [user, setUser] = useState(null);
+  const [userpic, setUserpic] = useState("");
 
-const [user, setUser] = useState(null)
-const [userpic , setUserpic] = useState("")
-useEffect(()=>{
-  onAuthStateChanged((user)=>{
-setUser(user);
-setUserpic(user.photoURL);
-// console.log(user);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setUserpic(currentUser?.photoURL || "");
+    });
 
-  })
-})
-const MoblieNav = () => {
-  if(user){
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <nav className='lg:hidden visible fixed w-screen h-16 flex flex-col items-center justify-center top-[-4rem] right-[1px] bg-white rounded-md hidden'>
-      
+    <nav className={`lg:hidden fixed w-screen h-16 flex flex-col items-center justify-center ${user ? 'top-[-4rem] right-[1px] bg-white rounded-md' : ''}`}>
       <Image src={logo} alt="Logo" className="w-12 h-12" />
-PoriChoi
-<div className="w-full flex items-center justify-center gap-[40%]">
-<div className="userpic">
-<Image src={userpic|| Avatar } width={250} height={250} className='rounded-full'/>
-</div>
-<div className="search">
-<Link href="/search"><FaSearch size={25} /></Link>
-
-
-</div>
-</div>
-
-      </nav>
-  );} else{
-    <nav className='lg:hidden visible fixed w-screen h-16 flex flex-col items-center justify-center'>
+      <span>PoriChoi</span>
       
-    <Image src={logo} alt="Logo" className="w-12 h-12" />
-PoriChoi
-<div className="w-full flex items-center justify-center gap-[40%]">
-<div className="userpic">
-<Image src={Avatar } width={250} height={250} className='rounded-full'/>
-</div>
-<div className="search">
-<Link href="/search"><FaSearch size={25} /></Link>
-
-</div>
-</div>
-
+      <div className="w-full flex items-center justify-center gap-[40%]">
+        <div className="userpic">
+          <Image 
+            src={userpic || Avatar} 
+            width={250} 
+            height={250} 
+            className='rounded-full'
+            alt="User profile"
+          />
+        </div>
+        <div className="search">
+          <Link href="/search">
+            <FaSearch size={25} />
+          </Link>
+        </div>
+      </div>
     </nav>
-  }
-}
+  );
+};
 
-export default MoblieNav;
+export default MobileNav;
