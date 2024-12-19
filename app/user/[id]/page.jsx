cfe4553f05c11a700f52e97061c0ac6921d3e47c
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-19 13:20:07 +0530

    Har Har Mahadev

--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -8,7 +8,7 @@ import NavBar from "../../Components/NavBar";
 import Image from 'next/image';
 import Post from '@/app/Components/Post';
 import { onAuthStateChanged } from 'firebase/auth';
-import MobileNav from '../../Components/MobileNav';
+import MobileNav from '../../Components/Moblie Nav';
 import { toast, ToastContainer } from 'react-toastify';
 
 const Page = () => {
