d62c31956bb5f6db38eb3f996fb426561d5bb7eb
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-21 23:37:15 +0530

    Har Har Mahadev

--- app/my-account/settings/page.jsx
+++ app/my-account/settings/page.jsx
@@ -30,7 +30,7 @@ const Page = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
   const [countryCode, setCountryCode] = useState("");
-
+  const [updatedphoto, setUpdatedPhoto] = useState(Avatar);
   const [changedImage, setChangedImage] = useState(null);
   const [changedName, setChangedName] = useState("");
   const [changedEmail, setChangedEmail] = useState("");
@@ -55,6 +55,10 @@ const Page = () => {
     const file = e.target.files[0];
     if (file) {
       setChangedImage(file);
+      const render = new FileReader()
+      render.onload = () => {
+        setUpdatedPhoto(render.result)
+      }
     }
   };
 
@@ -133,11 +137,14 @@ const Page = () => {
       <div className="top h-[30vh] w-screen bg-[#fff] flex items-center justify-center gap-4">
         <Image src={photo} width={100} height={100} className='rounded-full p-[10px]' alt='Profile Image' />
         <h1 className='text-2xl font-semibold'>{name}</h1>
-      </div>
+      </div>  
       <div className="flex w-screen h-screen items-center items-center justify-center ">
            <form className="flex flex-col gap-4 h-max justify-center items-center" onSubmit={handleSubmit}>
             <div className="text-3xl font-semibold">Update Your Profile</div>
             <b className='w-[30vw]'>ℹ️ You can also update one thing by just filling the input and clicking on Update.</b>
+        <Image src={updatedphoto} width={100} height={100} className='rounded-full p-[10px]' alt='Profile Image' />
+        <input type="file" onChange={handleImageUpload} className={inputClassName} />
+
             <input type="text" placeholder="Name" value={changedName || name} onChange={(e) => setChangedName(e.target.value)} className={inputClassName} />
             <input type="email" placeholder="Email" value={changedEmail || email} onChange={(e) => setChangedEmail(e.target.value)} className={inputClassName} />
             <div className="flex gap-2 items-center">
@@ -151,8 +158,7 @@ const Page = () => {
               </select>
               <input type="tel" placeholder="Phone Number" value={changedPhoneNumber || phoneNumber} onChange={(e) => setChangedPhoneNumber(e.target.value)} className={inputClassName} />
             </div>
-            <label>Photo:</label>
-            <input type="file" onChange={handleImageUpload} className={inputClassName} />
+            {/* <label>Photo:</label> */}
             <input type="submit" value="Update" className='lg:w-[10vw] lg:h-[6vh] w-max h-[6vh] rounded-md p-1 bg-[#0f0f0f] p-[10px] text-white hover:scale-110 cursor-pointer' />
          <div className="block mt-8">
             <div className="text-3xl">Delete Account</div>
