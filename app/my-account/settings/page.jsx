28742e938105bc53475c50024a43185f7f6a6c0c
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-22 19:58:20 +0530

    Har Har Mahadev

--- app/my-account/settings/page.jsx
+++ app/my-account/settings/page.jsx
@@ -76,6 +76,7 @@ const Page = () => {
         phoneNumber: fullPhoneNumber,
       };
 
+      console.log(countryCode)
       await updateProfile(user, updatedProfileData);
 
       await updateDoc(doc(firestore, 'userFollowingdata', user.uid), {
