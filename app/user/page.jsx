eb32107cc43bade5d10af37f407fe0bae3a15089
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-23 12:04:32 +0530

    Har Har Mahadev

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-con  page = () => {
+const  page = () => {
   return (
     <div className='w-screen h-screen flex items-center justify-center font-bold text-5xl'>
 User Data is Showed Here!
