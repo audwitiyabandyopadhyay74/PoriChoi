0935c6e2ccf855d48ab882ac48a8485c35af028f
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-27 13:29:20 +0530

    Har Har Mahadev

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -1,7 +1,8 @@
 'use client';
 
 import React from 'react';
-
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const SideBar = () => {
   const optionClassName = "h-[5vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer";
@@ -14,6 +15,8 @@ const SideBar = () => {
         <li className={optionClassName}><i className="fa-solid fa-phone"></i> Verify Phone Number</li>
         <li className={optionClassName}><i className="fa-solid fa-phone"></i> Reset Password</li>
         <li className={optionClassName}><i className="fa-solid fa-trash"></i> Delete Account</li>
+        
+        <li className={optionClassName + "bg-red-600"} onClick={() => signOut(auth)}><i className="fa-solid fa-right-from-bracket"></i> Logout</li>
       </ul>
     </div>
   );
