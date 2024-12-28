4f13f07e877925c85aa12e0edd9ebe483dbb07b7
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-28 13:10:06 +0530

    Har Har  Mahadev

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -11,11 +11,11 @@ const SideBar = () => {
   return (
     <div className="flex items-start h-full w-[25%] border-r border-r-black">
       <ul className="flex flex-col items-center gap-[20px]">
-       <Link href={"https://porichoi.vercel.app/my-account/settings"}> <li className={optionClassName}><i className="fa-solid fa-user"></i> Profile</li></Link>
-       <Link href={"https://porichoi.vercel.app/my-account/settings/verify-email"}> <li className={optionClassName}><i className="fa-solid fa-envelope-circle-check"></i> Verify Email</li></Link>
-       <Link href={"https://porichoi.vercel.app/my-account/settings/verify-phone-number"}> <li className={optionClassName}><i className="fa-solid fa-phone"></i> Verify Phone Number</li></Link>
-       <Link href={"https://porichoi.vercel.app/my-account/settings/rest-password"}> <li className={optionClassName}><i className="fa-solid fa-phone"></i> Reset Password</li></Link>
-       <Link href={"https://porichoi.vercel.app/my-account/settings/delete-account"}> <li className={optionClassName}><i className="fa-solid fa-trash"></i> Delete Account</li></Link>
+       <Link href={"https://porichoiop.vercel.app/my-account/settings"}> <li className={optionClassName}><i className="fa-solid fa-user"></i> Profile</li></Link>
+       <Link href={"https://porichoiop.vercel.app/my-account/settings/verify-email"}> <li className={optionClassName}><i className="fa-solid fa-envelope-circle-check"></i> Verify Email</li></Link>
+       <Link href={"https://porichoiop.vercel.app/my-account/settings/verify-phone-number"}> <li className={optionClassName}><i className="fa-solid fa-phone"></i> Verify Phone Number</li></Link>
+       <Link href={"https://porichoiop.vercel.app/my-account/settings/rest-password"}> <li className={optionClassName}><i className="fa-solid fa-phone"></i> Reset Password</li></Link>
+       <Link href={"https://porichoiop.vercel.app/my-account/settings/delete-account"}> <li className={optionClassName}><i className="fa-solid fa-trash"></i> Delete Account</li></Link>
         
         <li className={optionClassName + "bg-red-600"} onClick={() => signOut(auth)}><i className="fa-solid fa-right-from-bracket"></i> Logout</li>
       </ul>
