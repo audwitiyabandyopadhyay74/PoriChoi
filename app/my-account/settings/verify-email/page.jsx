aae46ef39d1e851fdcc61d3c92d3f2a3bc0bd707
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-28 12:51:39 +0530

    Har Har Mahadev

--- app/my-account/settings/verify-email/page.jsx
+++ app/my-account/settings/verify-email/page.jsx
@@ -2,7 +2,8 @@
 
 import React, { useState, useEffect } from 'react';
 import NavBar from '../../../Components/NavBar';
-import { auth, sendEmailVerification, updateProfile, onAuthStateChanged } from 'firebase/auth';
+import { sendEmailVerification, updateProfile, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../../firebase';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../../style.css";
