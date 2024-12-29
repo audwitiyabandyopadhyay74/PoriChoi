42315b592ab6a25f590e321814da51b946bb0ceb
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2024-12-29 21:27:02 +0530

    Har Har Mahadev

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -4,56 +4,56 @@ import React, { useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 import Link from 'next/link';
-// import { usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 const SideBar = () => {
-  // const [profile, setProfile] = useState(false);
-  // const [verifyEmail, setVerifyEmail] = useState(false);
-  // const [verifyPhoneNumber, setVerifyPhoneNumber] = useState(false);
-  // const [resetPassword, setResetPassword] = useState(false);
-  // const [deleteAccount, setDeleteAccount] = useState(false);
-  // // const pathname = usePathname();
+  const [profile, setProfile] = useState(false);
+  const [verifyEmail, setVerifyEmail] = useState(false);
+  const [verifyPhoneNumber, setVerifyPhoneNumber] = useState(false);
+  const [resetPassword, setResetPassword] = useState(false);
+  const [deleteAccount, setDeleteAccount] = useState(false);
+  const pathname = usePathname();
 
-  // if (pathname === "/my-account/settings") {
-  //   setProfile(true);
-  // } if (pathname === "/my-account/settings/verify-email") {
-  //   setVerifyEmail(true);
-  // } if (pathname === "/my-account/settings/verify-phone-number") {
-  //   setVerifyPhoneNumber(true);
-  // } if (pathname === "/my-account/settings/rest-password") {
-  //   setResetPassword(true);
-  // } else {
-  //   setDeleteAccount(true);
-  // }
+  if (pathname === "/my-account/settings") {
+    setProfile(true);
+  } if (pathname === "/my-account/settings/verify-email") {
+    setVerifyEmail(true);
+  } if (pathname === "/my-account/settings/verify-phone-number") {
+    setVerifyPhoneNumber(true);
+  } if (pathname === "/my-account/settings/rest-password") {
+    setResetPassword(true);
+  } else {
+    setDeleteAccount(true);
+  }
 
   const optionClassName = "h-[15vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer";
-  // const optionClassNameActive = "h-[15vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer bg-gray-200";
+  const optionClassNameActive = "h-[15vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer bg-gray-200";
 
   return (
     <div className="flex items-start h-full w-[25%] border-r border-r-black">
       <ul className="flex flex-col items-center gap-[20px]">
         <Link href="/my-account/settings">
-          <li className={optionClassName}>
+          <li className={profile?optionClassNameActive:optionClassName}>
             <i className="fa-solid fa-user"></i> Profile
           </li>
         </Link>
         <Link href="/my-account/settings/verify-email">
-          <li className={optionClassName}>
+          <li className={verifyEmail?optionClassNameActive:optionClassName}>
             <i className="fa-solid fa-envelope-circle-check"></i> Verify Email
           </li>
         </Link>
         <Link href="/my-account/settings/verify-phone-number">
-          <li className={optionClassName}>
+          <li className={verifyPhoneNumber?optionClassNameActive:optionClassName}>
             <i className="fa-solid fa-phone"></i> Verify Phone Number
           </li>
         </Link>
         <Link href="/my-account/settings/rest-password">
-          <li className={optionClassName}>
+          <li className={resetPassword?optionClassNameActive:optionClassName}>
             <i className="fa-solid fa-phone"></i> Reset Password
           </li>
         </Link>
         <Link href="/my-account/settings/delete-account">
-          <li className={ optionClassName}>
+          <li className={deleteAccount?optionClassNameActive:optionClassName}>
             <i className="fa-solid fa-trash"></i> Delete Account
           </li>
         </Link>
