af9413d7561a82ac3a06d635e5401e569c6b2a4b
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-01 21:03:55 +0530

    Har Har Mahadev

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 // import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 
 const SideBar = () => {
   const [profile, setProfile] = useState(false);
@@ -14,17 +14,17 @@ const SideBar = () => {
   const [deleteAccount, setDeleteAccount] = useState(false);
   const pathname = usePathname();
 
-  if (pathname === "/my-account/settings") {
-    setProfile(true);
-  } if (pathname === "/my-account/settings/verify-email") {
-    setVerifyEmail(true);
-  } if (pathname === "/my-account/settings/verify-phone-number") {
-    setVerifyPhoneNumber(true);
-  } if (pathname === "/my-account/settings/rest-password") {
-    setResetPassword(true);
-  } else {
-    setDeleteAccount(true);
-  }
+  // if (pathname === "/my-account/settings") {
+  //   setProfile(true);
+  // } if (pathname === "/my-account/settings/verify-email") {
+  //   setVerifyEmail(true);
+  // } if (pathname === "/my-account/settings/verify-phone-number") {
+  //   setVerifyPhoneNumber(true);
+  // } if (pathname === "/my-account/settings/rest-password") {
+  //   setResetPassword(true);
+  // } else {
+  //   setDeleteAccount(true);
+  // }
 
   const optionClassName = "h-[15vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer";
   const optionClassNameActive = "h-[15vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer bg-gray-200";
@@ -33,27 +33,27 @@ const SideBar = () => {
     <div className="flex items-start h-full w-[25%] border-r border-r-black">
       <ul className="flex flex-col items-center gap-[20px]">
         <a href="/my-account/settings">
-          <li className={profile?optionClassNameActive:optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-user"></i> Profile
           </li>
         </a>
         <a href="/my-account/settings/verify-email">
-          <li className={verifyEmail?optionClassNameActive:optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-envelope-circle-check"></i> Verify Email
           </li>
         </a>
         <a href="/my-account/settings/verify-phone-number">
-          <li className={verifyPhoneNumber?optionClassNameActive:optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-phone"></i> Verify Phone Number
           </li>
         </a>
         <a href="/my-account/settings/rest-password">
-          <li className={resetPassword?optionClassNameActive:optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-phone"></i> Reset Password
           </li>
         </a>
         <a href="/my-account/settings/delete-account">
-          <li className={deleteAccount?optionClassNameActive:optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-trash"></i> Delete Account
           </li>
         </a>
