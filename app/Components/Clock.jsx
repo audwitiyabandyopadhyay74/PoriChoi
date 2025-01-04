18a8fe5692ae45d1f2edb7175e4a5ea4549bd9e0
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-04 14:42:21 +0530

    Har Har Mahadev

--- app/Components/Clock.jsx
+++ app/Components/Clock.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "clock.css";
+import "./clock.css";
 const Clock = () => {
   return (
     <div className='w-[400rem] h-[100rem] rounded-full border clock' style={{borderStyle:"curve"}}>
