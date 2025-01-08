09b2ccb6396b767c9e3fe50e199190f30ed23d50
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-08 11:35:28 +0530

    Har Har Mahadev

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 export default {
     images: {
         domains: ['lh3.googleusercontent.com',
-            'firebasestorage.googleapis.com',
             "via.placeholder.com",
             "graph.facebook.com",
             'raw.githubusercontent.com'
