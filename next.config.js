304d8e42d94f38bc303a7525cc447672a63d8d07
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-08 12:43:28 +0530

    Har Har Mahadev

--- next.config.js
+++ next.config.js
@@ -8,12 +8,5 @@ export default {
             'raw.githubusercontent.com'
         ],
     },
-    reactStrictMode: true,
-    async redirects() {
-        return [{
-            source: '/',
-            destination: '/index', // replace 'index' with your actual main page file if it's not named 'index.js'
-            permanent: true,
-        }, ];
-    },
+
 }
