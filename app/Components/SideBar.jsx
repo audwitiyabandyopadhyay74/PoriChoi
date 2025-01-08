b149c060276d344a4837bcd8d48bf9250e009089
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-08 10:23:18 +0530

    Har Har Mahadev

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -27,33 +27,33 @@ const SideBar = () => {
   // }
 
   const optionClassName = "h-[7.5vh] w-[30vh] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer rounded-md";
-  const optionClassNameActive = "h-[15vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer bg-gray-200";
+  // const optionClassNameActive = "h-[15vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer bg-gray-200";
 
   return (
     <div className="flex items-start h-full w-[25%] border-r border-r-black">
       <ul className="flex flex-col items-center gap-[20px]">
         <a href="/my-account/settings">
-          <li className={profile ? optionClassNameActive : optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-user"></i> Profile
           </li>
         </a>
         <a href="/my-account/settings/verify-email">
-          <li className={verifyEmail ? optionClassNameActive : optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-envelope-circle-check"></i> Verify Email
           </li>
         </a>
         <a href="/my-account/settings/verify-phone-number">
-          <li className={verifyPhoneNumber ? optionClassNameActive : optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-phone"></i> Verify Phone Number
           </li>
         </a>
         <a href="/my-account/settings/rest-password">
-          <li className={resetPassword? optionClassNameActive : optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-lock"></i> Reset Password
           </li>
         </a>
         <a href="/my-account/settings/delete-account">
-          <li className={deleteAccount ? optionClassNameActive : optionClassName}>
+          <li className={optionClassName}>
             <i className="fa-solid fa-trash"></i> Delete Account
           </li>
         </a>
