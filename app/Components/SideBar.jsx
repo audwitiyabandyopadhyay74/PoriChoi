8bbe05889d120e2cd10260c5c10cae4b006969e1
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-08 10:10:48 +0530

    Har Har Mahadev

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -4,27 +4,27 @@ import React from 'react';
 import {  signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 // import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 
 const SideBar = () => {
-  const [profile, setProfile] = useState(false);
-  const [verifyEmail, setVerifyEmail] = useState(false);
-  const [verifyPhoneNumber, setVerifyPhoneNumber] = useState(false);
-  const [resetPassword, setResetPassword] = useState(false);
-  const [deleteAccount, setDeleteAccount] = useState(false);
-  const pathname = usePathname();
+  // const [profile, setProfile] = useState(false);
+  // const [verifyEmail, setVerifyEmail] = useState(false);
+  // const [verifyPhoneNumber, setVerifyPhoneNumber] = useState(false);
+  // const [resetPassword, setResetPassword] = useState(false);
+  // const [deleteAccount, setDeleteAccount] = useState(false);
+  // const pathname = usePathname();
 
-  if (pathname === "/my-account/settings") {
-    setProfile(true);
-  } if (pathname === "/my-account/settings/verify-email") {
-    setVerifyEmail(true);
-  } if (pathname === "/my-account/settings/verify-phone-number") {
-    setVerifyPhoneNumber(true);
-  } if (pathname === "/my-account/settings/rest-password") {
-    setResetPassword(true);
-  } else {
-    setDeleteAccount(true);
-  }
+  // if (pathname === "/my-account/settings") {
+  //   setProfile(true);
+  // } if (pathname === "/my-account/settings/verify-email") {
+  //   setVerifyEmail(true);
+  // } if (pathname === "/my-account/settings/verify-phone-number") {
+  //   setVerifyPhoneNumber(true);
+  // } if (pathname === "/my-account/settings/rest-password") {
+  //   setResetPassword(true);
+  // } else {
+  //   setDeleteAccount(true);
+  // }
 
   const optionClassName = "h-[7.5vh] w-[30vh] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer rounded-md";
   const optionClassNameActive = "h-[15vh] w-[95%] flex items-center justify-center gap-4 p-[30px] shadow-md cursor-pointer bg-gray-200";
