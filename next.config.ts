f1e1c4a4a14cd8097603126e13448fd5e156ea42
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-10 20:37:26 +0530

    Har Har Mahadev

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com',
-            'firebasestorage.googleapis.com',
-            "via.placeholder.com",
-            "graph.facebook.com",
-            'raw.githubusercontent.com'
-        ],
-    },
+  images: {
+      domains: ['lh3.googleusercontent.com',
+        'yt3.ggpht.com',
+          'firebasestorage.googleapis.com',
+          "via.placeholder.com",
+          "graph.facebook.com",
+          'raw.githubusercontent.com'
+      ],
+  },
 
-    reactStrictMode: true,
-    swcMinify: true,
+  reactStrictMode: true,
 }
 
 export default nextConfig
