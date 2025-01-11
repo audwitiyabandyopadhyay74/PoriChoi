fbe63942986159e357ac24ac95382746a48294af
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-11 17:45:51 +0530

    Har  Har Mahadev

--- app/not-found.js
+++ app/not-found.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
 export default function Custom404() {
-    return ( <
-        >
-        <
-        div className = 'w-screen h-screen flex flex-col items-center justify-center' >
-        <
-        span className = "text-5xl font-bold" > 404 < /span> <
-        span className = 'text-2xl' > Inconvenience Is Regretted! < /span> < /
-        div > <
-        />) 
+    return ( <>
+
+    <div className = 'w-screen h-screen flex flex-col items-center justify-center' >
+    <span className = "text-5xl font-bold" > 404 </span> 
+        <span className = 'text-2xl'> Inconvenience Is Regretted! </span> </div> 
+        </>) 
     }
