0119e5af75e696b9b5606ef15510601852823673
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-11 17:41:07 +0530

    Har Har Mahadev

--- app/not-found.js
+++ app/not-found.js
@@ -1,13 +1,13 @@
-// import React from 'react';
+import React from 'react';
 
-// export default function Custom404() {
-//     return ( <
-//         >
-//         <
-//         div className = 'w-screen h-screen flex flex-col items-center justify-center' >
-//         <
-//         span className = "text-5xl font-bold" > 404 < /span> <
-//         span className = 'text-2xl' > Inconvenience Is Regretted! < /span> < /
-//         div > <
-//         />) 
-//     }
+export default function Custom404() {
+    return ( <
+        >
+        <
+        div className = 'w-screen h-screen flex flex-col items-center justify-center' >
+        <
+        span className = "text-5xl font-bold" > 404 < /span> <
+        span className = 'text-2xl' > Inconvenience Is Regretted! < /span> < /
+        div > <
+        />) 
+    }
