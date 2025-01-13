90923d02924fdcf60e69bdb8fcdecd96de4cb4ab
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-13 11:07:25 +0530

    Har Har Mahadev

--- app/my-account/page.jsx
+++ app/my-account/page.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import Avatar from '../download.png';
 import Post from '../Components/Post';
 import { collection, getDocs } from 'firebase/firestore';
-import MoblieNav from '../Components/MoblieNav';
+import MoblieNav from '../Components/Moblie Nav';
 import { toast } from 'react-toastify';
 
 const Page = () => {
