37663451f4a3904ed6b98ab947fbbb297724ac6e
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-02-03 22:35:27 +0530

    Har Har Mahadev

--- app/Components/Scroll Progressbar.jsx
+++ app/Components/Scroll Progressbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+
 import { motion, useScroll } from "motion/react";
 
 const ScrollProgressbar = () => {
   const scrollYProgress = useScroll().scrollYProgress;
-
   return (
     <motion.div
-      className="w-full lg:h-24 h-1  mt-[]  bg-[#ff5c00] transition-none"
+      className="w-full lg:h-24 h-1  mt-80  bg-[#ff5c00] transition-none"
       style={{ scaleX: scrollYProgress, position: "fixed" }}
     ></motion.div>
   );
